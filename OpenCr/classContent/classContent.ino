42a5d4027d9490bc46abb0cfaa04944b18d85c01
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2025-10-23 11:42:02 +0200

    Update classContent.ino

--- OpenCr/classContent/classContent.ino
+++ OpenCr/classContent/classContent.ino
@@ -2,6 +2,11 @@
 #include "songs.h"
 #include "sensors.h"
 
+int criticalNote = NOTE_C4;
+float criticalVoltage = 11.5 ;
+
+unsigned long lastBatteryCheck = 0;
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Launching IMU");
@@ -11,23 +16,41 @@ void setup() {
   pinMode( led_pin, OUTPUT );
 }
 
+bool checkCriticalBattery(){
+  float voltage = getBatteryVoltage();
+  Serial.println("Executed: checkCriticalBattery ");
+
+  if (voltage < criticalVoltage) {
+    tone(BDPIN_BUZZER, criticalNote, 100);
+    return true;
+  }
+
+  return false;
+}
+
 void loop() {
 
     float roll = getRoll();
     float yaw = getYaw();
     float pitch = getPitch();
 
+    bool criticalVoltage = false;
+
     float voltage = getBatteryVoltage();
     
-    Serial.println(voltage);
-
+    // Controleer of er 60 seconden voorbij zijn sinds de laatste batterijcheck
+    if (millis() - lastBatteryCheck >= 60000) {
+        lastBatteryCheck = millis();           // Reset timer
+        criticalVoltage = checkCriticalBattery();  // Controleer opnieuw
+    }
+    
     Serial.print("Roll:");
     Serial.print(roll);
     Serial.print(",Pitch:");
     Serial.print(pitch);
     Serial.print(",Yaw:");
-    Serial.println(yaw);
+    Serial.print(yaw);
+    Serial.print(",Voltage:");
+    Serial.println(voltage);
     delay(100);  
-
-
 }
